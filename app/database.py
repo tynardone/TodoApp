41debe9dfd77da4cf202b99dafbb4a9fd61daac9
Tyler Nardone <tynardone@gmail.com>
2025-01-08 12:44:24 -0500

    Switching to SQLLITE

--- app/database.py
+++ app/database.py
@@ -2,9 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQL_ALCHEMY_DATABASE = (
-    "postgresql://tylernardone:test1234!@localhost/TodoApplicationDatabase"
-)
+# SQL_ALCHEMY_DATABASE = (
+#     "postgresql://tylernardone:test1234!@localhost/TodoApplicationDatabase"
+# )
+
+SQL_ALCHEMY_DATABASE = "sqlite:///./TodoApp.db"
 
 engine = create_engine(SQL_ALCHEMY_DATABASE)
 
